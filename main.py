730fb472dc01d6accb65dc6be462eaab2ae673d8
e7r74 <127944702+e7r74@users.noreply.github.com>
2023-12-12 09:45:58 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,83 @@
+print("1/line 1")
+# import json
+# ide = {}
+# ide['idea'] = [
+#     {
+#     "activity": "Create a cookbook with your favorite recipes",
+#     "type": "cooking",
+#     "participants": 1,
+#     "price": 0,
+#     "link": "",
+#     "key": "1947449",
+#     "accessibility": 0.05
+# }
+# ]
+# print(ide)
+
+
+
+print("2/line19")
+
+# import random
+
+# def add_random_to_dict(data):
+  
+#     random_key = f"random_{random.randint(1, 1000)}"
+#     random_value = random.randint(1, 1000)
+#     data[random_key] = random_value
+#     return data
+
+# activity_data = {
+#     "activity": "Create a cookbook with your favorite recipes",
+#     "type": "cooking",
+#     "participants": 1,
+#     "price": 0,
+#     "link": "",
+#     "key": "1947449",
+#     "accessibility": 0.05
+# }
+
+# activity_data_with_random = add_random_to_dict(activity_data.copy())
+
+# print(f"Исходный словарь: {activity_data}")
+# print(f"Словарь с добавленным случайным значением: {activity_data_with_random}")
+
+
+
+print("3/line 47")
+
+# import random
+
+# def random_line(file_path: str) -> str:
+#     with open(file_path, 'r') as file:
+#         lines = file.readlines()
+#         return random.choice(lines)
+# random_line = random_line('text.txt')
+# print(random_line)
+
+print ("4/line 58")
+# import json
+
+# data = {
+#     "activity": "Create a cookbook with your favorite recipes",
+#     "type": "cooking",
+#     "participants": 1,
+#     "price": 0,
+#     "link": "",
+#     "key": "1947449",
+#     "accessibility": 0.05
+# }
+
+# json_string = json.dumps(data)
+# replaced_string = json_string.replace("a", "D")
+
+# data_with_replaced_A = json.loads(replaced_string)
+
+# print(data_with_replaced_A)
+print ("5/line77")
+# def line_by_line(file_path: str) -> None:
+#     with open(file_path, 'r') as file:
+#         for line in file:
+#             print(line.strip())
+            
+# line_by_line('text.txt')
